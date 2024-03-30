092326f165b3fca44da7866c100243c3b41875dd
Minye Zhang <minyez.physchem@gmail.com>
2024-03-30 16:30:55 +0100

    update

--- macos/initial_cache.sonoma-gcc-mpich-openblas.debug.cmake
+++ macos/initial_cache.sonoma-gcc-mpich-openblas.debug.cmake
@@ -1,17 +1,18 @@
 set(CMAKE_Fortran_COMPILER "mpifort" CACHE STRING "" FORCE)
 # debug flags: -fcheck=all -fbacktrace
-set(CMAKE_Fortran_FLAGS "-g -O0 -fbacktrace -fallow-argument-mismatch -ffree-line-length-none -Wl,-ld_classic -Wl,-lstdc++" CACHE STRING "" FORCE)
-set(Fortran_MIN_FLAGS "-g -O0 -fbacktrace -fallow-argument-mismatch -ffree-line-length-none -Wl,-ld_classic" CACHE STRING "" FORCE)
+set(CMAKE_Fortran_FLAGS "-g -Wall -O0 -fbacktrace -fallow-argument-mismatch -ffree-line-length-none -Wl,-ld_classic -Wl,-lstdc++" CACHE STRING "" FORCE)
+set(Fortran_MIN_FLAGS "-g -Wall -O0 -fbacktrace -fallow-argument-mismatch -ffree-line-length-none -Wl,-ld_classic" CACHE STRING "" FORCE)
 set(CMAKE_C_COMPILER "mpicc" CACHE STRING "" FORCE)
-set(CMAKE_C_FLAGS "-cc=gcc-13 -g -O0 -Wl,-ld_classic" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS "-cc=gcc-13 -g -Wall -O0 -Wl,-ld_classic" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER "mpicxx" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "-cxx=g++-13 -g -O0 -Wl,-ld_classic" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-cxx=g++-13 -g -Wall -O0 -Wl,-ld_classic" CACHE STRING "" FORCE)
 set(LIB_PATHS "/opt/packages/scalapack/2.2.0/gcc-13.2.0-mpich-4.1.2-openblas /opt/homebrew/Cellar/openblas/0.3.26/lib" CACHE STRING "")
 set(LIBS "scalapack openblas" CACHE STRING "" FORCE)
 # for GreenX to use the same library
 set(BLAS_LIBRARIES "-L/opt/homebrew/Cellar/openblas/0.3.26/lib -lblas" CACHE STRING "")
 set(LAPACK_LIBRARIES "-L/opt/homebrew/Cellar/openblas/0.3.26/lib -llapack" CACHE STRING "")
 
+set(USE_GREENX ON CACHE BOOL "" FORCE)
 set(USE_MPI ON CACHE BOOL "" FORCE)
 set(USE_SCALAPACK ON CACHE BOOL "" FORCE)
 set(USE_LIBXC ON CACHE BOOL "" FORCE)
