ef1cf86b9cb788ef0e4237d4eee00c799a7a097f
Minye Zhang <minyez.physchem@gmail.com>
2024-07-15 09:42:24 +0200

    update fedora gcc samples

---
+++ y9kfed/initial_cache.y9kfed.gcc-mpich-openblas.debug.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_Fortran_COMPILER "mpifort" CACHE STRING "" FORCE)
+# debug flags: -fcheck=all -fbacktrace
+set(CMAKE_Fortran_FLAGS "-g -O0 -fallow-argument-mismatch -ffree-line-length-none -Wl,-lstdc++" CACHE STRING "" FORCE)
+set(Fortran_MIN_FLAGS "-g -O0 -fallow-argument-mismatch -ffree-line-length-none" CACHE STRING "" FORCE)
+set(CMAKE_C_COMPILER "mpicc" CACHE STRING "" FORCE)
+set(CMAKE_C_FLAGS "-g -O0" CACHE STRING "" FORCE)
+set(CMAKE_CXX_COMPILER "mpicxx" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-g -O0" CACHE STRING "" FORCE)
+set(LIB_PATHS "/opt/packages/scalapack/2.2.0/gcc-13.2.1-mpich-4.1.2-openblas /opt/packages/openblas/0.3.26/gcc-13.2.1/lib" CACHE STRING "")
+set(LIBS "scalapack openblas" CACHE STRING "" FORCE)
+# for GreenX to use the same library
+set(BLAS_LIBRARIES "-L/opt/packages/openblas/0.3.26/gcc-13.2.1/lib -lblas" CACHE STRING "")
+set(LAPACK_LIBRARIES "-L/opt/packages/openblas/0.3.26/gcc-13.2.1/lib -llapack" CACHE STRING "")
+
+set(USE_GREENX ON CACHE BOOL "" FORCE)
+set(USE_MPI ON CACHE BOOL "" FORCE)
+set(USE_SCALAPACK ON CACHE BOOL "" FORCE)
+set(USE_LIBXC ON CACHE BOOL "" FORCE)
+set(USE_HDF5 OFF CACHE BOOL "" FORCE)
+set(USE_RLSY ON CACHE BOOL "" FORCE)
+set(ELPA2_KERNEL "" CACHE STRING "Change to AVX/AVX2/AVX512 if running on Intel processors" FORCE)
