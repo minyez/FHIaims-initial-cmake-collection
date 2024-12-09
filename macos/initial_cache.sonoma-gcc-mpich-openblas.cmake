81c0f100f257bf670afe63a3474abf9fd867b60c
Minye Zhang <minyez.physchem@gmail.com>
2024-12-09 15:47:35 +0100

    add intel debug cache

--- macos/initial_cache.sonoma-gcc-mpich-openblas.cmake
+++ macos/initial_cache.sonoma-gcc-mpich-openblas.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER "mpicc" CACHE STRING "" FORCE)
 set(CMAKE_C_FLAGS "-cc=gcc-14 -O3 -Wl,-ld_classic" CACHE STRING "" FORCE)
 set(CMAKE_CXX_COMPILER "mpicxx" CACHE STRING "" FORCE)
 set(CMAKE_CXX_FLAGS "-cxx=g++-14 -O3 -Wl,-ld_classic" CACHE STRING "" FORCE)
-set(LIB_PATHS "/opt/packages/scalapack/2.2.0/gcc-14.2.0-mpich-4.2.2-openblas /opt/homebrew/Cellar/openblas/0.3.28/lib" CACHE STRING "")
+set(LIB_PATHS "/opt/packages/scalapack/2.2.0/gcc-14.2.0_1-mpich-4.2.3-openblas /opt/homebrew/Cellar/openblas/0.3.28/lib" CACHE STRING "")
 set(LIBS "scalapack openblas" CACHE STRING "" FORCE)
 # for GreenX to use the same library
 set(BLAS_LIBRARIES "-L/opt/homebrew/Cellar/openblas/0.3.28/lib -lblas" CACHE STRING "")
